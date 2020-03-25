4b8e1a224871df0f3bef114ff53ce5e44a04e40b
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-25 13:07:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ class App < Sinatra::Base
   
   post '/team' do 
     @name = params["name"]
-    @coach = params[:name]
-    @pg = params[:name]
-    @sg = params[:name]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
     @pf = params[:name]
     @sf = params[:name]
     @c = params[:name]
