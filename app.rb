8bbf392758669bb22d78ec79a74f9ae978d1d1ff
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-25 13:01:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+  
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/team' do 
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params[:name]
+    @sg = params[:name]
+    @pf = params[:name]
+    @sf = params[:name]
+    @c = params[:name]
+    
+    erb :team
+  end
 end
