91c2873413ab52b48737a50dfbd159258e569259
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-25 13:07:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     @pg = params["pg"]
     @sg = params["sg"]
     @pf = params["pf"]
-    @sf = params[:name]
+    @sf = params["sf"]
     @c = params[:name]
     
     erb :team
